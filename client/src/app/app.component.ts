288b59e73e0e9a99f98024b661cbd8ae14788388
Marcelo Eduardo <twekieoficial@gmail.com>
2018-05-28 12:27:59 -0300

    Add files via upload

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map, catchError } from 'rxjs/operators';
+import { environment } from '@env/environment';
+import { throwError } from 'rxjs';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  })
+};
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  submitted: Boolean = false;
+  authForm: FormGroup;
+  messagesList: Array<any> = [];
+  constructor(
+    private http: HttpClient,
+    private _fb: FormBuilder
+  ) {
+    this.authForm = this._fb.group({
+      'message': ['', [Validators.required, Validators.minLength(3)]]
+    });
+  }
+  botMessage(value: string) {
+    return this.searchMessage(value).pipe(
+      map(res => {
+        console.log(res);
+        this.messagesList.push(res['data']);
+      }),
+      catchError(err => {
+        console.log(err);
+        return throwError(new Error());
+      })
+    );
+  }
+  searchMessage(value: string) {
+    return this.http.get(`${environment.api}/bot?message=${value}`, httpOptions);
+  }
+  get f() {
+    return this.authForm.controls;
+  }
+  onSubmit(value) {
+    this.submitted = true;
+    if (this.authForm.invalid) {
+      return;
+    }
+    const message = value['message'];
+    this.botMessage(message).subscribe();
+  }
+}
---
+++ client/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
